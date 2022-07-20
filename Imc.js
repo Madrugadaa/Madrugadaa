aa1f03ba3c4cd6e42897ca79671d256bb6b923a2
Madrugadaa <109670348+Madrugadaa@users.noreply.github.com>
2022-07-20 08:36:30 -0400

    Create Imc.js

---
+++ Imc.js
@@ -0,0 +1,10 @@
+var nome = "Maria";
+var peso = 63.1;
+var altura = 1.58;
+var imc = peso / altura * altura;
+
+if (imc >= 30){
+    console.log(nome + ",você está acima do peso")
+    }else{
+    console.log(nome + ",você está no peso ideal")
+}
